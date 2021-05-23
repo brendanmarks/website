e6aaa16c6e68491452ac396a4f4aa2f8b5449154
Brendan Marks <brendan99@live.ca>
2021-05-23 15:26:18 -0400

    Updates

---
+++ precache-manifest.5bf70e5d889142c87a3d3449910e6808.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "489e0d377ee30d200570",
+    "url": "/website/static/css/main.46b74db1.chunk.css"
+  },
+  {
+    "revision": "489e0d377ee30d200570",
+    "url": "/website/static/js/main.489e0d37.chunk.js"
+  },
+  {
+    "revision": "5c5726c62be19c3eab9a",
+    "url": "/website/static/css/1.e2a0c8e3.chunk.css"
+  },
+  {
+    "revision": "5c5726c62be19c3eab9a",
+    "url": "/website/static/js/1.5c5726c6.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "d4d5c6d070f666b0e5738f5e1853098b",
+    "url": "/website/static/media/BrendanMarksResume2021.d4d5c6d0.pdf"
+  },
+  {
+    "revision": "ae352fa86cfa72d647bd0186afc1834e",
+    "url": "/website/static/media/me.ae352fa8.png"
+  },
+  {
+    "revision": "0fc3076ba9e955dcf910d72885a42b63",
+    "url": "/website/static/media/rbc.0fc3076b.png"
+  },
+  {
+    "revision": "27611b6dcdf88d34156b1bbce9ff9b3d",
+    "url": "/website/static/media/matrox.27611b6d.jpg"
+  },
+  {
+    "revision": "2bdf43a3927e60f51c546bbbde20121b",
+    "url": "/website/index.html"
+  }
+];
