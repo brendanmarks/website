d0eabbf5df375a7794c6d0f1a42380eb9bcb3f24
Brendan Marks <brendan99@live.ca>
2021-05-23 16:01:08 -0400

    fix image size

--- src/components/Accomplishment.js
+++ src/components/Accomplishment.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import {Image, StyleSheet} from 'react-native';
 import ScrollAnimation from 'react-animate-on-scroll';
 import '../CSS/Accomplishment.css';
 
 class Accomplishment extends Component {
+    
     render(){
+        const styles = StyleSheet.create({
+            imageStyle: {
+                alignSelf: 'center',
+                height:'100%', 
+                width:'100%'
+              }
+          });
         const {link, image, alt, title, subtitle1, subtitle2, description} = this.props;
         return (
             <ScrollAnimation animateIn="fadeIn" animateOnce>
                 <div className="accomplishment">
-                    <a className="card" target="_blank" rel="noopener noreferrer" href={link}><img alt={alt} src={image}></img></a>
+                    <a className="card" target="_blank" rel="noopener noreferrer" href={link}><Image style={styles.imageStyle} resizeMode={'contain'} alt={alt} source={image}></Image></a>
                     <div className="accomplishment-info">
                         <p className="accomplishment-title">{title}</p>
                         <p className="accomplishment-subtitle1">{subtitle1}</p>
